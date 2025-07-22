9f4e4a597e451ea3bf529ee289bff9c0c40e72b4
nXs <nxs88@mail.ru>
2025-07-22 17:09:11 +0700

    Starting project

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,11 @@
 import { MantineProvider } from '@mantine/core';
 import '@mantine/core/styles.css';
 import './App.scss';
-import CardList from './components/CardList';
-import type { Launch } from './types/Launch';
-import { useFetch } from './hooks/useFetch';
 
 function App() {
-  const { data } = useFetch<Launch[]>(
-    'https://api.spacexdata.com/v3/launches?launch_year=2020'
-  );
   return (
     <MantineProvider>
-      <div className="container">
-        <CardList data={data || []} />
-      </div>
+      <div className="container"></div>
     </MantineProvider>
   );
 }
