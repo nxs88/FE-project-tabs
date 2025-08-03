7cc80dd12f66f26ab7c9a7439cb11a4b65cd99af
nXs <nxs88@mail.ru>
2025-08-04 01:16:24 +0700

    Попытка исправить дефолтный 404 на GH

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,12 @@
 /// <reference types="vitest" />
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { ghPages } from 'vite-plugin-gh-pages';
 
 // https://vite.dev/config/
 export default defineConfig({
   base: '/FE-project-tabs/',
-  plugins: [react()],
+  plugins: [react(), ghPages()],
   test: {
     setupFiles: ['./setupTests.ts'],
     globals: true,
